40d5eafe4ac56a3b0b6ae23c7a95ae46970c7ed1
Drew Hilario <72259687+andrewhilario@users.noreply.github.com>
2023-03-12 00:32:46 +0800

    Add files via upload

--- settings.py
+++ settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     #Django Auto Refresh
     "django_browser_reload.middleware.BrowserReloadMiddleware",
+
 ]
 
 ROOT_URLCONF = 'pos_systemv2.urls'
