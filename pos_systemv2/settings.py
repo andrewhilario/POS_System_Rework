9af79970bf285731a36c854ec2837ee2b947780d
Drew <hilarioandrew12@gmail.com>
2024-10-12 20:43:02 +0800

    Updated

--- pos_systemv2/settings.py
+++ pos_systemv2/settings.py
@@ -154,12 +154,12 @@ CLOUDINARY_STORAGE = {
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 STATIC_URL = "/static/"
-MEDIA_URL = "/images/"
+# MEDIA_URL = "/images/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
+# MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
